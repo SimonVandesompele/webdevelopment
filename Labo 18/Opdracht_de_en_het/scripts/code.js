ca2dc0486e4207c6399ff5a9d8d03dd8c013dbb8
SimonVandesompele <115065783+SimonVandesompele@users.noreply.github.com>
2023-03-19 09:46:53 +0100

    Labo 18

--- Labo 18/Opdracht_de_en_het/scripts/code.js
+++ Labo 18/Opdracht_de_en_het/scripts/code.js
@@ -1,10 +1,10 @@
 const setup = () => {
     let tekst = "Gisteren zat de jongen op de stoep en at de helft van de appel";
     let tekstSplit = tekst.split(" ");
-
+    console.log(tekst);
     for (let i = 0; i < tekstSplit.length; i++)
     {
-        if(tekstSplit[i] == "de")
+        if(tekstSplit[i] === "de")
         {
             tekstSplit[i] = "het"
         }
